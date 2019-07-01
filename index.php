19132f48f247b150c3ee5c15920205c2a68de76a
1 <g.ponsard@simplon-charleville.fr>
2019-07-01 15:59:24 +0200

    ajout images mascotte en index

--- index.php
+++ index.php
@@ -8,6 +8,7 @@
     
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
         integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.7.2/animate.min.css">
         <link rel="stylesheet" href="css/reset.css">
         <link rel="stylesheet" href="css/style.css">
 
@@ -17,9 +18,20 @@
 <body>
 <?php include("header.php") ; ?>
 
+<!--///////////////////MAIN////////////////////////-->
+<main>
+<img class="animated bounceInOUT" src="img/eclair.png">
+
+
+
+
+
+
     <!--/////////////////////SECTION ENTETE///////////////////////-->
 
-    <section class=entete></section>
+    <section class=entete>
+       <img class="animated bounceInUp" src="img/mascotte.png">
+    </section>
 
 
     <!--/////////////////////SECTION PRESENTATION///////////////////////-->
@@ -532,7 +544,7 @@
             </a>
     </section>
 
-
+    </main>
 
 
     <!--/////////////////////FOOTER///////////////////////-->
