1bec90a8e79257e680d61f6e9698bdd665512726
jcollart <j.collart@simplon-charleville.fr>
2019-07-02 15:46:51 +0200

    modif header

--- header.php
+++ header.php
@@ -21,7 +21,7 @@
 
 <body>
   <!-- NAVBAR BOOTSTRAP-->
-  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <nav class="mynavbar navbar navbar-expand-lg navbar-light bg-light">
     <a class="navbar-brand" href="http://www.ardenne-metropole.fr"><img id="logo" src="img/logoetudiant.png" alt="logo"/></a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
       aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
