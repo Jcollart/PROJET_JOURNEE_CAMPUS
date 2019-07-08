56c0e9b6c384c7fa7839a5366a2ab481eb377f3c
[jerome] <[j.collart@simplon-charleville.fe]>
2019-07-08 16:18:54 +0200

    chgmt header

--- header1.php
+++ header1.php
@@ -22,42 +22,42 @@
 <li class="hvr-grow"><a href="index.php"><i style="font-size:3em" class="fas fa-home"></i></a></li><!-- n1 -->
 <li><a href="#">Activitées</a><!-- n1 -->
 	<ul>
-		<li><a href="#">CAROLO EXPRESS <img src="img/multi.png"  border="0" width="20" height="20" align="absmiddle"></a><!-- n2 -->
+		<li><a href="#">carolo express <img src="img/multi.png"  border="0" width="20" height="20" align="absmiddle"></a><!-- n2 -->
 			<ul>
-				<li><a href="#collapseOne">Description activitée</a></li><!-- n3 -->
-				<li><a href="formulexpress.php">Formulaire d'inscription</a></li><!-- n3 -->
+				<li><a href="#collapseOne">Description</a></li><!-- n3 -->
+				<li><a href="formulexpress.php">Inscription</a></li><!-- n3 -->
 			</ul>
 		</li>
-		<li><a href="#">CAROLO PIQUE-NIQUE <img src="img/multi.png" border="0" width="15" height="15" alt="" align="absmiddle"></a><!-- n2 -->
+		<li><a href="#">carolo pique-nique <img src="img/multi.png" border="0" width="15" height="15" alt="" align="absmiddle"></a><!-- n2 -->
 			<ul>
-				<li><a href="#collapseTwo">Description réservation</a></li><!-- n3 -->
-				<li><a href="formulpiquenique.php">formulaire d'inscription</a></li><!-- n3 -->
+				<li><a href="#collapseTwo">Description</a></li><!-- n3 -->
+				<li><a href="formulpiquenique.php">Inscription</a></li><!-- n3 -->
 			</ul>
         </li>
-        <li><a href="#">CAROLO WARRIOR <img src="img/multi.png" border="0" width="15" height="15" alt="" align="absmiddle"></a><!-- n2 -->
+        <li><a href="#">carolo warrior<img src="img/multi.png" border="0" width="15" height="15" alt="" align="absmiddle"></a><!-- n2 -->
 			<ul>
-				<li><a href="#collapseThree">Description activitée</a></li><!-- n3 -->
-				<li><a href="formulwarrior.php">formulaire d'inscription</a></li><!-- n3 -->
+				<li><a href="#collapseThree">Description</a></li><!-- n3 -->
+				<li><a href="formulwarrior.php">Inscription</a></li><!-- n3 -->
 			</ul>
         </li>
-        <li><a href="#">CAROLO CUISINE <img src="img/multi.png" border="0" width="15" height="15" alt="" align="absmiddle"></a><!-- n2 -->
+        <li><a href="#">carolo cuisine <img src="img/multi.png" border="0" width="15" height="15" alt="" align="absmiddle"></a><!-- n2 -->
 			<ul>
-				<li><a href="#collapseFour">Description activités</a></li><!-- n3 -->
-				<li><a href="formulcuisine.php">formulaire d'inscription</a></li><!-- n3 -->
+				<li><a href="#collapseFour">Description</a></li><!-- n3 -->
+				<li><a href="formulcuisine.php">Inscription</a></li><!-- n3 -->
 			</ul>
 		</li>
 		
 	</ul>
 </li>
 <li class="">  
-          <a href="https://www.charleville-mezieres.fr/"><img id="logocm" src="img/Logos/logomairieCM.png" alt="logocm"/></a>
+         <a href="https://www.charleville-mezieres.fr/"><img id="logocm" src="img/Logos/logomairieCM.png" alt="logocm"/></a>
         </li>
 
 </li>	
 <li><a href="galerie.php">Galerie photos</a></li>
 <li><a href="#formulcontact"><i style="font-size:1em" class="fas fa-user"></i>Contact</a></li>
 <li><form class="Fbarre">
-<input type="search" name "barre" id"champ1" value=" rechercher ..."</input>
+<input type="search" name "barre" id"champ1" value=" rechercher ..."/>
 <button onlick= 'rechercher()'><i style="font-size:1em" class="fas fa-search"></i></button></form></li></ul>
 
 </header>
