0183dc661b8285598a01da5507d682938f362a39
jerome.collart <j.collart@simplon-charleville.fr>
2019-09-02 11:56:48 +0200

    modif card temoignage

--- temoignages.php
+++ temoignages.php
@@ -1,159 +1,162 @@
- <div class="titre"><h2>TEMOIGNAGES</h2></div>
-        <div id="carouselTemoignages" class="carousel slide" data-ride="carousel">
-            <div class="carousel-inner">
-            <div class="carousel-item active">
-                    <div class="card mb-3" id="mb">
-                        <div class="row no-gutters">
-                            <div class="col-md-4">
-                                <img src="img/01.jpg" class="card-img1"
-                                    alt="La rentrée des étudiants - Témoignage ">
-                            </div>
-                            <div class="col-md-8">
-                                <div class="card-body">
-                                    <p class="card-text"> « Une amie m’a convaincu et je n’ai pas regretté. J’ai
-                                        rencontré d’autres étudiants, c’était sympa ce brassage. Je n’ai pas pu faire
-                                        les activités mais j’ai encouragé les équipes de mon école et on a gagné. »</p>
-                                       <p class="card-text"> Mareme – 18 ans</p>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="carousel-item">
-                    <div class="card mb-3" id="mb">
-                        <div class="row no-gutters">
-                            <div class="col-md-4">
-                                <img src="img/01.jpg" class="card-img1"
-                                    alt="La rentrée des étudiants - Témoignage ">
-                            </div>
-                            <div class="col-md-8">
-                                <div class="card-body">
-                                    <p class="card-text"> « J’ai apprécié cette journée et toutes les villes devraient
-                                        organiser ce genre de manifestation pour les étudiants. Moi j’ai participé au
-                                        Carolo Express, c’était fun ! »</p>
-                                    <p class="card-text">Jérémy – 21 ans</p>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="carousel-item">
-                    <div class="card mb-3" id="mb">
-                        <div class="row no-gutters">
-                            <div class="col-md-4">
-                                <img src="img/01.jpg" class="card-img1"
-                                    alt="La rentrée des étudiants - Témoignage ">
-                            </div>
-                            <div class="col-md-8">
-                                <div class="card-body">
-                                    <p class="card-text">« Le Carolo Warrior ! Des épreuves fantastiques
-                                      et quelle ambiance ! Des étudiants qui crient le slogan de leur équipe,
-                                      des banderoles de soutien ! Une vraie dynamique de la Rentrée étudiante à Charleville-Mézières ! »</p>
-                                    <p class="card-text">Kylian - 22 ans</p>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="carousel-item">
-                    <div class="card mb-3" id="mb">
-                        <div class="row no-gutters">
-                            <div class="col-md-4">
-                                <img src="img/01.jpg" class="card-img1" alt="La rentrée des étudiants - Témoignage ">
-                            </div>
-                            <div class="col-md-8">
-                                <div class="card-body">
-                                    <p class="card-text"> « Superbe journée ! J’ai notamment adoré le Carolo Express qui
-                                        m’a permis de découvrir des lieux que je ne connaissais pas à Charleville-Mézières,
-                                        c’était super intéressant. Je recommande sans problème ! »</p>
-                                    <p class="card-text">Claire - 19 ans</p>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="carousel-item">
-                    <div class="card mb-3" id="mb">
-                        <div class="row no-gutters">
-                            <div class="col-md-4">
-                                <img src="img/01.jpg" class="card-img1" alt="La rentrée des étudiants - Témoignage ">
-                            </div>
-                            <div class="col-md-8">
-                                <div class="card-body">
-                                    <p class="card-text"> « TOP !!! Je compte bien me réinscrire cette année… et gagner
-                                        le GROS LOT <i class="fas fa-smile"></i> »</p>
-                                    <p class="card-text">Jaahed – 19 ans</p>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="carousel-item">
-                    <div class="card mb-3" id="mb">
-                        <div class="row no-gutters">
-                            <div class="col-md-4">
-                                <img src="img/01.jpg" class="card-img1" alt="La rentrée des étudiants - Témoignage ">
-                            </div>
-                            <div class="col-md-8">
-                                <div class="card-body">
-                                    <p class="card-text"> « Pour ma part, j’ai participé au Concours de cuisine sur la Place Ducale ! <br>
-                                                           Bonne ambiance et bon challenge à relever pour ceux qui aiment cuisiner. <br>
-                                                           Et puis, avoir comme cuisine le cadre de la magnifique Place Ducale, ça le fait !
-                                        Merci encore aux organisateurs et notamment à Romain pour sa patience, j’en garde un très bon souvenir ! »
-                                    </p>
-                                    <p class="card-text">Mélanie – 21 ans</p>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="carousel-item">
-                    <div class="card mb-4">
-                        <div class="row no-gutters">
-                            <div class="col-md-12">
-                                <div class="card-body align-items-center">
-                                    <p class="card-text"> « J’ai apprécié cette journée et toutes les villes devraient
-                                        organiser ce genre de manifestation pour les étudiants. Moi j’ai participé au
-                                        Carolo Express, c’était fun ! »</p>
-                                    <p class="card-text">Jérémy – 21 ans</p>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="carousel-item">
-                    <div class="card mb-4">
-                        <div class="row no-gutters">
-                            <div class="col-md-12">
-                                <div class="card-body">
-                                    <p class="card-text"> « Superbe journée ! J’ai notamment adoré le Carolo Express qui
-                                        m’a permis de découvrir des lieux que je ne connaissais pas à Charleville,
-                                        c’était super intéressant. Je recommande sans problème ! »</p>
-                                    <p class="card-text">Claire - 19ans</p>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="carousel-item">
-                    <div class="card mb-4">
-                        <div class="row no-gutters">
-                            <div class="col-md-12">
-                                <div class="card-body align-items-center">
-                                    <p class="card-text"> « TOP !!! Je compte bien me réinscrire cette année… et gagner
-                                        le GROS LOT <i class="fas fa-smile"></i> #GUIGUI MONEY FROM NEUILLY»</p>
-                                    <p class="card-text">Jaahed – 19 ans</p>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>-->
-            </div>
-            <a class="carousel-control-prev" href="#carouselTemoignages" role="button" data-slide="prev">
-                <span class="carousel-control-prev-icon" id="carousel-control-prev-icon" aria-hidden="true"></span>
-                <span class="sr-only">Previous</span>
-            </a>
-            <a class="carousel-control-next" href="#carouselTemoignages" role="button" data-slide="next">
-                <span class="carousel-control-next-icon" id="carousel-control-next-icon" aria-hidden="true"></span>
-                <span class="sr-only">Next</span>
-            </a>
+ <div class="titre">
+     <h2>TEMOIGNAGES</h2>
+ </div>
+ <div id="carouselTemoignages" class="carousel slide" data-ride="carousel">
+     <div class="carousel-inner">
+         <div class="carousel-item active">
+             <div class="card mb-3" id="mb">
+                 <div class="row no-gutters">
+                     <div class="col-md-4">
+                         <img src="img/01.jpg" class="card-img1" alt="La rentrée des étudiants - Témoignage ">
+                     </div>
+                     <div class="col-md-8">
+                         <div class="card-body">
+                             <p class="card-text"> « Une amie m’a convaincu et je n’ai pas regretté. J’ai
+                                 rencontré d’autres étudiants, c’était sympa ce brassage. Je n’ai pas pu faire
+                                 les activités mais j’ai encouragé les équipes de mon école et on a gagné. »</p>
+                             <p class="card-text"> Mareme – 18 ans</p>
+                         </div>
+                     </div>
+                 </div>
+             </div>
+         </div>
+         <div class="carousel-item">
+             <div class="card mb-3" id="mb">
+                 <div class="row no-gutters">
+                     <div class="col-md-4">
+                         <img src="img/01.jpg" class="card-img1" alt="La rentrée des étudiants - Témoignage ">
+                     </div>
+                     <div class="col-md-8">
+                         <div class="card-body">
+                             <p class="card-text"> « J’ai apprécié cette journée et toutes les villes devraient
+                                 organiser ce genre de manifestation pour les étudiants. Moi j’ai participé au
+                                 Carolo Express, c’était fun ! »</p>
+                             <p class="card-text">Jérémy – 21 ans</p>
+                         </div>
+                     </div>
+                 </div>
+             </div>
+         </div>
+         <div class="carousel-item">
+             <div class="card mb-3" id="mb">
+                 <div class="row no-gutters">
+                     <div class="col-md-4">
+                         <img src="img/01.jpg" class="card-img1" alt="La rentrée des étudiants - Témoignage ">
+                     </div>
+                     <div class="col-md-8">
+                         <div class="card-body">
+                             <p class="card-text">« Le Carolo Warrior ! Des épreuves fantastiques
+                                 et quelle ambiance ! Des étudiants qui crient le slogan de leur équipe,
+                                 des banderoles de soutien ! Une vraie dynamique de la Rentrée étudiante à
+                                 Charleville-Mézières ! »</p>
+                             <p class="card-text">Kylian - 22 ans</p>
+                         </div>
+                     </div>
+                 </div>
+             </div>
+         </div>
+         <div class="carousel-item">
+             <div class="card mb-3" id="mb">
+                 <div class="row no-gutters">
+                     <div class="col-md-4">
+                         <img src="img/01.jpg" class="card-img1" alt="La rentrée des étudiants - Témoignage ">
+                     </div>
+                     <div class="col-md-8">
+                         <div class="card-body">
+                             <p class="card-text"> « Superbe journée ! J’ai notamment adoré le Carolo Express qui
+                                 m’a permis de découvrir des lieux que je ne connaissais pas à Charleville-Mézières,
+                                 c’était super intéressant. Je recommande sans problème ! »</p>
+                             <p class="card-text">Claire - 19 ans</p>
+                         </div>
+                     </div>
+                 </div>
+             </div>
+         </div>
+         <div class="carousel-item">
+             <div class="card mb-3" id="mb">
+                 <div class="row no-gutters">
+                     <div class="col-md-4">
+                         <img src="img/01.jpg" class="card-img1" alt="La rentrée des étudiants - Témoignage ">
+                     </div>
+                     <div class="col-md-8">
+                         <div class="card-body">
+                             <p class="card-text"> « TOP !!! Je compte bien me réinscrire cette année… et gagner
+                                 le GROS LOT <i class="fas fa-smile"></i> »</p>
+                             <p class="card-text">Jaahed – 19 ans</p>
+                         </div>
+                     </div>
+                 </div>
+             </div>
+         </div>
+         <div class="carousel-item">
+             <div class="card mb-3" id="mb">
+                 <div class="row no-gutters">
+                     <div class="col-md-4">
+                         <img src="img/01.jpg" class="card-img1" alt="La rentrée des étudiants - Témoignage ">
+                     </div>
+                     <div class="col-md-8">
+                         <div class="card-body">
+                             <p class="card-text"> « Pour ma part, j’ai participé au Concours de cuisine sur la Place
+                                 Ducale ! <br>
+                                 Bonne ambiance et bon challenge à relever pour ceux qui aiment cuisiner. <br>
+                                 Et puis, avoir comme cuisine le cadre de la magnifique Place Ducale, ça le fait !
+                                 Merci encore aux organisateurs et notamment à Romain pour sa patience, j’en garde un
+                                 très bon souvenir ! »
+                             </p>
+                             <p class="card-text">Mélanie – 21 ans</p>
+                         </div>
+                     </div>
+                 </div>
+             </div>
+         </div>
+         <!--    <div class="carousel-item">
+             <div class="card mb-4">
+                 <div class="row no-gutters">
+                     <div class="col-md-12">
+                         <div class="card-body align-items-center">
+                             <p class="card-text"> « J’ai apprécié cette journée et toutes les villes devraient
+                                 organiser ce genre de manifestation pour les étudiants. Moi j’ai participé au
+                                 Carolo Express, c’était fun ! »</p>
+                             <p class="card-text">Jérémy – 21 ans</p>
+                         </div>
+                     </div>
+                 </div>
+             </div>
+         </div>
+         <div class="carousel-item">
+             <div class="card mb-4">
+                 <div class="row no-gutters">
+                     <div class="col-md-12">
+                         <div class="card-body">
+                             <p class="card-text"> « Superbe journée ! J’ai notamment adoré le Carolo Express qui
+                                 m’a permis de découvrir des lieux que je ne connaissais pas à Charleville,
+                                 c’était super intéressant. Je recommande sans problème ! »</p>
+                             <p class="card-text">Claire - 19ans</p>
+                         </div>
+                     </div>
+                 </div>
+             </div>
+         </div>
+         <div class="carousel-item">
+             <div class="card mb-4">
+                 <div class="row no-gutters">
+                     <div class="col-md-12">
+                         <div class="card-body align-items-center">
+                             <p class="card-text"> « TOP !!! Je compte bien me réinscrire cette année… et gagner
+                                 le GROS LOT <i class="fas fa-smile"></i> #GUIGUI MONEY FROM NEUILLY»</p>
+                             <p class="card-text">Jaahed – 19 ans</p>
+                         </div>
+                     </div>
+                 </div>
+             </div>
+         </div>-->
+     </div>
+ </div>
+ <a class="carousel-control-prev" href="#carouselTemoignages" role="button" data-slide="prev">
+     <span class="carousel-control-prev-icon" id="carousel-control-prev-icon" aria-hidden="true"></span>
+     <span class="sr-only">Previous</span>
+ </a>
+ <a class="carousel-control-next" href="#carouselTemoignages" role="button" data-slide="next">
+     <span class="carousel-control-next-icon" id="carousel-control-next-icon" aria-hidden="true"></span>
+     <span class="sr-only">Next</span>
+ </a>
